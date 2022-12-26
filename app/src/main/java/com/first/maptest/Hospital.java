bfeaa30e3d50c6e6dc2449be4518338fda06fc5d
4seokhyeon <tktjrgus@naver.com>
2022-12-26 21:50:01 +0900

    First Commit

---
+++ app/src/main/java/com/first/maptest/Hospital.java
@@ -0,0 +1,42 @@
+package com.first.maptest;
+
+public class Hospital {
+    private String profile;
+    private String addr;
+    private String h_name;
+    private String h_type;
+
+    public Hospital(){}
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getH_name() {
+        return h_name;
+    }
+
+    public void setH_name(String h_name) {
+        this.h_name = h_name;
+    }
+
+    public String getH_type() {
+        return h_type;
+    }
+
+    public void setH_type(String h_type) {
+        this.h_type = h_type;
+    }
+}
---
+++ app/src/main/java/com/first/maptest/MainActivity.java
@@ -0,0 +1,95 @@
+package com.first.maptest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapOptions;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.util.FusedLocationSource;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+
+
+
+        locationSource =
+                new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+
+        CameraPosition cameraPosition = new CameraPosition(
+                new LatLng(35.1798159, 129.0750222), // 대상 지점
+                16, // 줌 레벨
+                20, // 기울임 각도
+                0 // 베어링 각도
+        );
+
+
+        NaverMapOptions options = new NaverMapOptions()
+                .camera(cameraPosition)
+                .mapType(NaverMap.MapType.Terrain)
+                .enabledLayerGroups(NaverMap.LAYER_GROUP_BUILDING)
+                .compassEnabled(true)
+                .scaleBarEnabled(true)
+                .locationButtonEnabled(true);
+
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance(options);
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            return;
+        }
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+
+        naverMap.setOnMapClickListener((point, coord) ->
+                Toast.makeText(this, getString(R.string.format_map_click, coord.latitude, coord.longitude),
+                        Toast.LENGTH_SHORT).show());
+
+        naverMap.setOnMapLongClickListener((point, coord) ->
+                Toast.makeText(this, getString(R.string.format_map_long_click, coord.latitude, coord.longitude),
+                        Toast.LENGTH_SHORT).show());
+
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setLocationButtonEnabled(true);
+
+    }
+}
