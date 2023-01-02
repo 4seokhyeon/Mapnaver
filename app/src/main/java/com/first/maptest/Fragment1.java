3fe2e0a25363ab1182f2d8a1f45f2cbe983afe01
4seokhyeon <tktjrgus@naver.com>
2023-01-02 17:43:05 +0900

    맵 프래그먼트 넣었음 연결 해보 실 분 급구

--- app/src/main/java/com/first/maptest/Fragment1.java
+++ app/src/main/java/com/first/maptest/Fragment1.java
@@ -5,26 +5,99 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.CameraPosition;
 import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.NaverMapOptions;
+import com.naver.maps.map.OnMapReadyCallback;
 
-public class Fragment1 extends Fragment {
+public class Fragment1 extends Fragment implements OnMapReadyCallback {
+    //지도 객체 변수
+    private MapView mapView;
     private View view;
+
+    public Fragment1() { }
+
+    public static Fragment1 newInstance()
+    {
+        Fragment1 fragment = new Fragment1();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_1,
+                container, false);
 
-        view=inflater.inflate(R.layout.fragment_1, container, false);
+        mapView = (MapView) rootView.findViewById(R.id.navermap);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
 
+        return rootView;
+    }
 
-        return view;
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        //배경 지도 선택
+        naverMap.setMapType(NaverMap.MapType.Satellite);
+
+        //건물 표시
+        naverMap.setLayerGroupEnabled(naverMap.LAYER_GROUP_BUILDING, true);
+
+        //위치 및 각도 조정
+        CameraPosition cameraPosition=new CameraPosition(new LatLng(33.38,126.55),9,45,45);
+
+        naverMap.setCameraPosition(cameraPosition);
+    }
+    @Override
+    public void onStart() {
+        String addr;
+        super.onStart();
+        mapView.onStart();
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        mapView.onPause();
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        mapView.onDestroy();
+    }
+    @Override
+    public void onLowMemory(){
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
