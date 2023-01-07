ea746e4544b95121f06b6bd6f43a416e39508a39
guswl <guswldpqj1@naver.com>
2023-01-07 23:11:35 +0900

    더보기탭 화면전환

--- app/src/main/java/com/first/maptest/fragment/Fragment2.java
+++ app/src/main/java/com/first/maptest/fragment/Fragment2.java
@@ -19,39 +19,50 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.first.maptest.R;
+import com.first.maptest.moretab.my;
+import com.first.maptest.reserve;
+
+import java.util.zip.Inflater;
 
 //예약 프래그 먼 한나가 해보렴 여기다가 자유롭게 할때 커밋 푸쉬 주석 생활화 하셔
 public class Fragment2 extends Fragment {
 
-    //
+    //예약버튼
+    public static Fragment2 newInstance() {
+        return new Fragment2();
+    }
+
    /*  Chronometer chrono;
     Button btnStart, btnEnd;
     RadioButton rdoCal,rdoTime;
     CalendarView calender;
     TimePicker time;
-    TextView tvYear, tvMonth, tvDay, tvHour, tvMinute;
+    TextView tvYear, tvMonth, tvDay, tvHour, tvMinute;*/
 
-    @override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    /* setContentView(R.layout.activity_main);
         setTitle("시간 예약");
 
-        chrono = (Chronometer)findViewById(R.id.chronometer1);
-        btnStart = (Button)findViewById(R.id.btnStart);
-        btnEnd = (Button)findViewById(R.id.btnEnd);
-        rdoCal = (RadioButton)findViewById(R.id.rdoCal);
-        rdoTime = (RadioButton)findViewById(R.id.rdoTime);
-        calendar = (CalendarView)findViewById(R.id.calendarView1);
-        time = (TimePicker)findViewById(R.id.timePicker1);
-        tvYear = (TextView)findViewById(R.id.tvYear);
-        tvMonth = (TextView)findViewById(R.id.tvMonth);
-        tvDay = (TextView)findViewById(R.id.tvDay);
-        tvHour = (TextView)findViewById(R.id.tvHour);
-        tvMinute = (TextView)findViewById(R.id.tvMinute);
+        chrono = (Chronometer) findViewById(R.id.chronometer1);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnEnd = (Button) findViewById(R.id.btnEnd);
+        rdoCal = (RadioButton) findViewById(R.id.rdoCal);
+        rdoTime = (RadioButton) findViewById(R.id.rdoTime);
+        calendar = (CalendarView) findViewById(R.id.calendarView1);
+        time = (TimePicker) findViewById(R.id.timePicker1);
+        tvYear = (TextView) findViewById(R.id.tvYear);
+        tvMonth = (TextView) findViewById(R.id.tvMonth);
+        tvDay = (TextView) findViewById(R.id.tvDay);
+        tvHour = (TextView) findViewById(R.id.tvHour);
+        tvMinute = (TextView) findViewById(R.id.tvMinute);
 
         calendar.setVisibility(View.INVISIBLE);
         time.setVisibility(View.INVISIBLE);
@@ -92,14 +103,25 @@ public class Fragment2 extends Fragment {
                 tvHour.setText(Integer.toString(time.getCurrentHour()));
                 tvMinute.setText(Integer.toString(time.getCurrentMinute()));
             }
-        }); 일단 날짜 시간 창*/
-
-    private View view;
+        });일단 날짜 시간 창*/
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false);
+        //예약버튼
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_2, container, false);
+
+        Button reserve = rootView.findViewById(R.id.reserve);
+        reserve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                reserve reserve = new reserve();
+                fragmentTransaction.replace(R.id.mainframe, reserve);
+                fragmentTransaction.commit();
+            }
+        });
+        return rootView;
     }
 }
---
+++ app/src/main/java/com/first/maptest/reserve.java
@@ -0,0 +1,46 @@
+package com.first.maptest;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.first.maptest.fragment.Fragment2;
+import com.first.maptest.fragment.Fragment5;
+import com.first.maptest.moretab.review;
+
+public class reserve extends Fragment {
+    public static reserve newInstance()
+    {
+        return new reserve();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.reserve, container, false);
+
+        Button back = rootView.findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                Fragment2 fragment2 = new Fragment2();
+                fragmentTransaction.replace(R.id.mainframe, fragment2);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return rootView;
+    }
+}
