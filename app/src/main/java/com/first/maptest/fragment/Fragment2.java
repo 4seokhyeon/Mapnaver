b2d6c71ab49aa50de012903da086017327cbaed0
phy7452 <phy7452@naver.com>
2023-01-08 20:03:28 +0900

    폼 연결 하는 중....

--- app/src/main/java/com/first/maptest/fragment/Fragment2.java
+++ app/src/main/java/com/first/maptest/fragment/Fragment2.java
@@ -36,74 +36,12 @@ public class Fragment2 extends Fragment {
         return new Fragment2();
     }
 
-   /*  Chronometer chrono;
-    Button btnStart, btnEnd;
-    RadioButton rdoCal,rdoTime;
-    CalendarView calender;
-    TimePicker time;
-    TextView tvYear, tvMonth, tvDay, tvHour, tvMinute;*/
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-    /* setContentView(R.layout.activity_main);
-        setTitle("시간 예약");
-
-        chrono = (Chronometer) findViewById(R.id.chronometer1);
-        btnStart = (Button) findViewById(R.id.btnStart);
-        btnEnd = (Button) findViewById(R.id.btnEnd);
-        rdoCal = (RadioButton) findViewById(R.id.rdoCal);
-        rdoTime = (RadioButton) findViewById(R.id.rdoTime);
-        calendar = (CalendarView) findViewById(R.id.calendarView1);
-        time = (TimePicker) findViewById(R.id.timePicker1);
-        tvYear = (TextView) findViewById(R.id.tvYear);
-        tvMonth = (TextView) findViewById(R.id.tvMonth);
-        tvDay = (TextView) findViewById(R.id.tvDay);
-        tvHour = (TextView) findViewById(R.id.tvHour);
-        tvMinute = (TextView) findViewById(R.id.tvMinute);
-
-        calendar.setVisibility(View.INVISIBLE);
-        time.setVisibility(View.INVISIBLE);
-
-        rdoCal.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                calendar.setVisibility(View.VISIBLE);
-                time.setVisibility(View.INVISIBLE);
-            }
-        });
-
-        rdoTime.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                calendar.setVisibility(View.INVISIBLE);
-                time.setVisibility(View.VISIBLE);
-            }
-        });
-
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                chrono.setBase(SystemClock.elapsedRealtime());
-                chrono.start();
-                chrono.setTextColor(Color.RED);
-            }
-        });
-
-        btnEnd.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                chrono.stop();
-                chrono.setTextColor(Color.BLUE);
-
-                java.util.Calendar curDate = java.util.Calendar.getInstance();
-                curDate.setTimeInMillis(calendar.getDate());
-                tvYear.setText(Integer.toString(curDate.get(Calendar.YEAR)));
-                tvMonth.setText(Integer.toString(curDate.get(Calendar.MONTH)));
-                tvDay.setText(Integer.toString(curDate.get(Calendar.DATE)));
-
-                tvHour.setText(Integer.toString(time.getCurrentHour()));
-                tvMinute.setText(Integer.toString(time.getCurrentMinute()));
-            }
-        });일단 날짜 시간 창*/
-    }
 
     @Override
    public View onCreateView(LayoutInflater inflater, ViewGroup container,
--- app/src/main/java/com/first/maptest/reserve.java
+++ app/src/main/java/com/first/maptest/reserve.java
@@ -1,10 +1,13 @@
 package com.first.maptest;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -15,6 +18,8 @@ import com.first.maptest.fragment.Fragment5;
 import com.first.maptest.moretab.review;
 
 public class reserve extends Fragment {
+
+
     public static reserve newInstance()
     {
         return new reserve();
@@ -24,6 +29,14 @@ public class reserve extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    //폼
+    public void ButtonClick2(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://naver.me/5OGvHS9D"));
+        startActivity(intent);
     }
 
     @Override
