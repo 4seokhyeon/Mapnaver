b3ccb4e56d617ef67bf940c5159724a66132ee73
phy7452 <phy7452@naver.com>
2023-05-24 23:59:40 +0900

    fragment2 내역 넣음

--- app/src/main/java/com/first/maptest/fragment/Fragment2.java
+++ app/src/main/java/com/first/maptest/fragment/Fragment2.java
@@ -1,48 +1,36 @@
 package com.first.maptest.fragment;
 
 
-
-import static android.content.ContentValues.TAG;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.app.Activity;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.os.SystemClock;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.Chronometer;
-import android.widget.RadioButton;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
-
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.first.maptest.R;
-import com.first.maptest.reserve;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.zip.Inflater;
 
 //예약 탭
 public class Fragment2 extends Fragment {
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String Uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    DocumentReference docuser = db.collection("Users").document(Uid);
+    DocumentReference docmv = db.collection("MV").document(Uid);
+    DocumentReference dochp = db.collection("Hp").document(Uid);
+    DocumentReference docconfirm = db.collection("confirm").document(Uid);
+
 
     TextView mv_date, mv_time, mv_ad, mv_name, hp_name, hp_ps, hp_time, hp_date;
+    TextView tv_name1, tv_date, tv_time, tv_hname;
+
 
     public static Fragment2 newInstance() {
         return new Fragment2();
@@ -67,8 +55,23 @@ public class Fragment2 extends Fragment {
         hp_ps = (TextView) rootView.findViewById(R.id.hp_ps);
         hp_date = (TextView) rootView.findViewById(R.id.hp_date);
         hp_time = (TextView) rootView.findViewById(R.id.hp_time);
+        tv_name1 = (TextView) rootView.findViewById(R.id.tv_name1);
+        tv_date = (TextView) rootView.findViewById(R.id.tv_date);
+        tv_hname = (TextView) rootView.findViewById(R.id.tv_hname);
+        tv_time = (TextView) rootView.findViewById(R.id.tv_time);
 
-        db.collection("Users")
+
+        docuser.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                //City city = documentSnapshot.toObject(City.class);
+                String name = documentSnapshot.getString("name");
+
+                mv_name.setText(name);
+                hp_name.setText(name);
+            }
+        });
+        /* db.collection("Users")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -83,9 +86,22 @@ public class Fragment2 extends Fragment {
                             Log.d(TAG,"Error getting documents: ", task.getException());
                         }
                     }
-                });
+                });*/
 
-        db.collection("MV")
+        docmv.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                //City city = documentSnapshot.toObject(City.class);
+                String ad = documentSnapshot.getString("ad");
+                String date = documentSnapshot.getString("date");
+                String time = documentSnapshot.getString("time");
+                mv_ad.setText("목적지: "+ad);
+                mv_date.setText("예약날짜: "+date);
+                mv_time.setText("예약시간: "+time);
+            }
+        });
+
+       /* db.collection("MV")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -104,8 +120,22 @@ public class Fragment2 extends Fragment {
                         }
                     }
                 });
+*/
 
-        db.collection("Hp")
+        dochp.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                //City city = documentSnapshot.toObject(City.class);
+                String ps = documentSnapshot.getString("ps");
+                String date = documentSnapshot.getString("date");
+                String time = documentSnapshot.getString("time");
+                hp_ps.setText("매니저: "+ps);
+                hp_date.setText("예약날짜: "+date);
+                hp_time.setText("예약시간: "+time);
+            }
+        });
+
+        /*db.collection("Hp")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -123,8 +153,48 @@ public class Fragment2 extends Fragment {
                             Log.d(TAG,"Error getting documents: ", task.getException());
                         }
                     }
-                });
+                });*/
+
+        docconfirm.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                //City city = documentSnapshot.toObject(City.class);
+                String name = documentSnapshot.getString("name");
+                String date = documentSnapshot.getString("date");
+                String time = documentSnapshot.getString("time");
+                String hname = documentSnapshot.getString("hname");
+                tv_name1.setText(name);
+                tv_date.setText("예약날짜: "+date);
+                tv_time.setText("예약시간: "+time);
+                tv_hname.setText("예약한 병원: "+hname);
+            }
+        });
 
         return rootView;
     }
 }
+
+/*
+        db.collection("confirm")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot document : task.getResult()){
+                                String name = document.getString("name");
+                                String date = document.getString("date");
+                                String time = document.getString("time");
+                                String hname = document.getString("hname");
+                                tv_name1.setText(name);
+                                tv_date.setText("예약날짜: "+date);
+                                tv_time.setText("예약시간: "+time);
+                                tv_hname.setText(hname);
+                            }
+                        }else{
+                            Log.d(TAG,"Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+*/
+
